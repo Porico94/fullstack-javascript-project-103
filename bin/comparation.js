708f90863c9932de47443d8baaa8fdfa974125ef
Porico94 <poolrimari@gmail.com>
2024-12-17 21:30:56 -0500

    Add Difference Calculator

---
+++ bin/comparation.js
@@ -0,0 +1,51 @@
+import _ from "lodash";
+
+const gendiff = (file1, file2) => {
+
+  let commonKeys = [];
+  let commonKeysArray = [];
+  let uncommonKeys1 = [];
+  let uncommonKeys2 = [];
+
+  const objectKeysFile1 = Object.keys(file1);
+  const objectKeysFile2 = Object.keys(file2);
+
+  //Comparamos los keys y values de los dos objetos y lo guardamos en un array
+  for (const key1 of objectKeysFile1) {
+    for (const key2 of objectKeysFile2) {
+      if (file1[key1] === file2[key2] && key1 === key2) {
+        commonKeys.push(key1);
+        commonKeysArray.push(`  ${key1}: ${file1[key1]}`);
+      }
+    }
+  }
+
+  //Guardamos los keys no comunes del primer objeto
+  for (const objectKey1 of objectKeysFile1) {
+    if (!commonKeys.includes(objectKey1)) {
+      uncommonKeys1.push(`- ${objectKey1}: ${file1[objectKey1]}`);
+    }
+  }
+
+  //Guardamos los keys no comunes del segundo objeto
+  for (const objectKey2 of objectKeysFile2) {
+    if (!commonKeys.includes(objectKey2)) {
+      uncommonKeys2.push(`+ ${objectKey2}: ${file2[objectKey2]}`);
+    }
+  }
+
+  //Unimos los arrays
+  const arrayResult = [...commonKeysArray, ...uncommonKeys1, ...uncommonKeys2];
+
+  //Ordenamos el array
+  const arrayResultOrdered = _.sortBy(arrayResult, (item) => {
+    const key = item.substring(2).split(':')[0];
+    const prefixPriority = item.startsWith('-') ? 0 : 1;
+    return [key, prefixPriority];
+  });
+
+  // Formateamos el resultado como un "objeto JSON" y lo devolvemos
+   return `{\n${arrayResultOrdered.join("\n")}\n}`;
+};
+
+export default gendiff;
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import readFile from './fileReader.js';
+import gendiff from './comparation.js';
 
 const program = new Command();
 
@@ -10,12 +11,9 @@ program
   .option('-f, --format <type>', 'output format')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .action((filepath1, filepath2) => {
-    console.log(`File 1 Path: ${filepath1}`);
-    console.log(`File 2 Path: ${filepath2}`);
+  .action((filepath1, filepath2) => {    
     const file1Data = readFile(filepath1);
     const file2Data = readFile(filepath2);
-    console.log(`${JSON.stringify(file1Data, null, 2)}`);
-    console.log(`${JSON.stringify(file2Data, null, 2)}`);
+    console.log(gendiff(file1Data, file2Data));
   })
   .parse(process.argv);
