468bf1f0b870b1f6da81a93619f93d415d5fd23b
Porico94 <poolrimari@gmail.com>
2025-02-24 13:24:02 -0500

    Fix eslint v9

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -12,9 +12,7 @@ const getIndentation = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCoun
 
 const formatValue = (data, depth, renderFunctions) => {
   if (!_.isObject(data)) return String(data);
-  const entries = Object.entries(data).map(([key, value]) =>
-    renderFunctions[UNCHANGED_VALUE]({ key, value }, depth + 1)
-  );
+  const entries = Object.entries(data).map(([key, value]) => renderFunctions[UNCHANGED_VALUE]({ key, value }, depth + 1));
   return `{\n${entries.join('\n')}\n${getIndentation(depth)}  }`;
 };
 
@@ -25,47 +23,21 @@ const renderFunctions = {
   },
   [NESTED_VALUE]: ({ key, children }, depth, iterate) => {
     const nestedChildren = children.flatMap((child) => iterate(child, depth + 1));
-    return `${getIndentation(depth)}  ${key}: {\n${nestedChildren.join('\n')}\n${getIndentation(
-      depth
-    )}  }`;
+    return `${getIndentation(depth)}  ${key}: {\n${nestedChildren.join('\n')}\n${getIndentation(depth)}  }`;
   },
-  [ADD_VALUE]: (node, depth) =>
-    `${getIndentation(depth)}+ ${node.key}: ${formatValue(
-      node.value,
-      depth,
-      renderFunctions,
-    )}`,
-  [DELETED_VALUE]: (node, depth) =>
-    `${getIndentation(depth)}- ${node.key}: ${formatValue(
-      node.value,
-      depth,
-      renderFunctions,
-    )}`,
-  [UNCHANGED_VALUE]: (node, depth) =>
-    `${getIndentation(depth)}  ${node.key}: ${formatValue(
-      node.value,
-      depth,
-      renderFunctions,
-    )}`,
+  [ADD_VALUE]: (node, depth) => `${getIndentation(depth)}+ ${node.key}: ${formatValue(node.value, depth, renderFunctions)}`,
+  [DELETED_VALUE]: (node, depth) => `${getIndentation(depth)}- ${node.key}: ${formatValue(node.value, depth, renderFunctions)}`,
+  [UNCHANGED_VALUE]: (node, depth) => `${getIndentation(depth)}  ${node.key}: ${formatValue(node.value, depth, renderFunctions)}`,
   [CHANGED_VALUE]: (node, depth) => {
     const { key, value1, value2 } = node;
-    const formattedValue1 = `${getIndentation(depth)}- ${key}: ${formatValue(
-      value1,
-      depth,
-      renderFunctions,
-    )}`;
-    const formattedValue2 = `${getIndentation(depth)}+ ${key}: ${formatValue(
-      value2,
-      depth,
-      renderFunctions,
-    )}`;
+    const formattedValue1 = `${getIndentation(depth)}- ${key}: ${formatValue(value1, depth, renderFunctions)}`;
+    const formattedValue2 = `${getIndentation(depth)}+ ${key}: ${formatValue(value2, depth, renderFunctions)}`;
     return [formattedValue1, formattedValue2].join('\n');
   },
 };
 
 const renderAST = (ast) => {
-  const iterate = (node, depth) =>
-    renderFunctions[node.type](node, depth, iterate);
+  const iterate = (node, depth) => renderFunctions[node.type](node, depth, iterate);
   return iterate(ast, 0);
 };
 
